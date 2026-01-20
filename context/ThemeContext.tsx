5a6d4fb3a9f0e1ae3f3e054a2b5f9817a5cacdc8
GitNimay <nimesh.kulkarni2004@gmail.com>
2026-01-20 23:32:53 +0530

    fixed logo

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -40,11 +40,6 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ childre
       } else {
         root.classList.remove('dark');
       }
-
-      // Update favicon to match theme
-      if (typeof (window as any).updateFavicon === 'function') {
-        (window as any).updateFavicon();
-      }
     };
 
     applyTheme();
